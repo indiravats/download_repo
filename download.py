2da664d4b7b4c4df3f2483c83cd3b92ce06bec73
Indira Vats <74598343+indiravats@users.noreply.github.com>
2022-10-06 00:07:45 +0530

    Update download.py

--- download.py
+++ download.py
@@ -3,17 +3,16 @@ import pandas as pd
 from git import Repo
 
 results = pd.read_csv('results.csv')
-results_compact = results.head(10)
 
 repo_list = []
 
-for i in range(len(results_compact)):
-  repo_list.append(results_compact.name[i])
+for i in range(len(results)):
+  repo_list.append(results.name[i])
 
-for i in range(len(results_compact)):
+for i in range(len(results)):
   folder_name = repo_list[i].split('/')
   x = 'https://github.com/' + repo_list[i]
-  location = '/Users/indiravats/PycharmProjects/download-repo/downloaded/repository_'+ folder_name[1]
+  location = '/data/indira/csl/download_repo/downloaded/repository'+ folder_name[1]
   Repo.clone_from(x, location)
   print("Downloading repository:", i+1)
 

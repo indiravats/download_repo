e6aabf8258fd2403a38c3acb744ce229c37c0f93
Indira Vats <74598343+indiravats@users.noreply.github.com>
2022-10-06 23:51:28 +0530

    Update download.py

--- download.py
+++ download.py
@@ -12,7 +12,7 @@ for i in range(len(results)):
 for i in range(len(results)):
   folder_name = repo_list[i].split('/')
   x = 'https://github.com/' + repo_list[i]
-  location = '/data/indira/csl/download_repo/downloaded/repository'+ folder_name[1]
+  location = '/data/indira/csl/download_repo/downloaded/'+ folder_name[1]
   Repo.clone_from(x, location)
   print("Downloading repository:", i+1)
 
